fb0f71453cfb1ce420a9602f7e545da2b9ba27ac
TarsorCase <gerardgonzalez41@gmail.com>
2024-06-04 15:03:05 -0400

    Empeazndo la logica de peticiones al usuario

--- ExamenFinal.cpp
+++ ExamenFinal.cpp
@@ -1,15 +1,40 @@
 #include <iostream>
 #include <map>
-#include <vector>
 #include <string>
+#include <sstream>
 
 using namespace std;
 
 struct Fecha{
-  
+  int year;
+  int month;
+  int day;
 };
 
-int int main () {
-  map<string>
+int main () {
+  map<Fecha, string> Info; 
+  string comandos;
+  string prt1;
+  string prt2;
+  string prt3;
+  bool cont = true;
+  while(cont){
+    getline(cin, comandos);
+    if(comandos.empty()) continue;
+    stringstream iss(comandos);
+    if(iss >> prt1){
+      if(prt1 == "Add"){
+        if(iss >> prt2 >> prt3){
+      
+      }
+      }else if(prt1 == "Del"){
+
+      }else if(prt1 == "Find"){
+
+      }else if(prt1 == "Print"){
+
+      }
+    }
+  }
   return 0;
 }
